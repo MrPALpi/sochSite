07ab97d349bf8e0003574093c95112d1cb2548a1
MrPALpi <kapralov-02@bk.ru>
2022-12-08 21:41:55 +0500

    correction of the addition

--- src/components/ReportForm.vue
+++ src/components/ReportForm.vue
@@ -3,7 +3,6 @@
     <form @submit.prevent>
             <h4>Загрузка отчёта</h4>
             
-            <!-- <button class = "btn" @click="createPost" >Создать</button> -->
             <uploadFile ref="file"
             @handleFileUpload="handleFileUpload">
 
@@ -29,12 +28,9 @@
             type="text"
             placeholder="Номер работы"></MyInput>
 
-            <!-- <MyInput 
-            v-model:value="report.author"
-            type="text"
-            placeholder="Автор"></MyInput> -->
 
-            <MySelect v-model="report.author_uuid" :options="names"> </MySelect>
+
+            <!-- <MySelect v-model="report.author_uuid" :options="names"> </MySelect> -->
 
             <MyInput 
             v-model:value="report.note"
@@ -45,10 +41,7 @@
             @click="createPost"
             style="margin-top: 15px;"
             >Создать</MyButton>
-            <!-- <MyButton
-            @click="fileText"
-            style="margin-top: 15px;"
-            >вывести файл</MyButton> -->
+
             
         </form>
     </div>
@@ -58,6 +51,7 @@
 import MyInput from "./UI/MyInput.vue";
 import uploadFile from '@/components/v-uploadFile.vue';
 import axios from "axios";
+import { mapState} from "vuex";
 export default {
     data() {
         return {
@@ -66,8 +60,7 @@ export default {
             report:{
                 title:"",
                 path:"",
-                author:"",
-                author_uuid:"174ccd93-9170-497a-9ecf-3632e13e846d",
+                author: '',
                 subject:"",
                 type_work:"",
                 number_work: "",
@@ -75,45 +68,30 @@ export default {
                 doc_uuid:""
             },
             file:'',
-            names : [
-                    {value: "174ccd93-9170-497a-9ecf-3632e13e846d", name: "Даниил"},
-                    {value: "d77af4cf-0136-4cce-9873-7f11309aeceb", name: "Роман"},
-                    {value: "1501dd0f-ba3e-4716-a263-d4ba1ca018fb", name: "Александр"},
-                    {value: "cf040460-0a8b-4178-aaa7-4e9f1c7607fc", name: "Андрей"},
-                    {value: "cf040460-0ddb-4178-aaa7-4ne41c7607fc", name: "Матвей"},
-
-                ]
-            // nameFields:[
-            // "Название",
-            // "Предмет",
-            // "Тип работы",
-            // "Номер работы",
-            // "Автор",
-            // "Дополнения",
-            // ]
         };
     },
     methods: {
         createPost() {
-            console.log("Автор до",this.report.author);
+            // console.log("Автор до",this.report.author);
+
             this.sendFile(this.report,this.file);
-            this.getUser(this.report,this.report.author_uuid);
-            // console.log("Смотри сюда ущерб",this.filePath)
-            console.log("Автор после",this.report.author);
+            // console.log("wtf",this.report.author);
+            // this.getUser(this.report,this.report.author_uuid);
+
+            // console.log("коченный?",this.globalAuthor, this.report.author);
             if (!this.report.title == "") {
                 this.$emit("create", this.report);
                 this.report = {
                     title:"",
                     path:"",
-                    author:"",
-                    author_uuid:"174ccd93-9170-497a-9ecf-3632e13e846d",
+                    author:this.globalAuthor,
                     subject:"",
                     type_work:"",
                     number_work: "",
                     note:"",
                     doc_uuid:""
                 };
-                
+                console.log("wtf",this.report.author);
                 // this.$emit('sendFile');
             }
         },
@@ -127,13 +105,12 @@ export default {
         
             
         async sendFile(report, file){
-                console.log(report);
+                report.author = this.globalAuthor
                 const formData = new FormData();
-                // console.log("ЗДЕСЬ",typeof formData);
                 formData.append('file',file);
                 formData.append('title',report.title);
                 formData.append('file_type', file.name.slice(file.name.indexOf(".")+1,));
-                formData.append('author_uuid',report.author_uuid);
+                formData.append('author_uuid',this.globalAuthor.uuid);
                 formData.append('subject',report.subject);
                 formData.append('type_work',report.type_work);
                 formData.append('number_work',report.number_work);
@@ -155,18 +132,18 @@ export default {
                 }
                 
         },
-        async getUser(report,user){
-                try{
-                    await axios.get(`roma/api/get?user.uuid=${user}`)
-                    .then(response => report.author=response['data']['users'][0]);
-                    console.log("ТУЧЕ", report.author);
-                    // this.report.author = response['data']['users'][0];
+        // async getUser(report,user){
+        //         try{
+        //             await axios.get(`roma/api/get?user.uuid=${user}`)
+        //             .then(response => report.author=response['data']['users'][0]);
+        //             console.log("ТУЧЕ", report.author);
+        //             // this.report.author = response['data']['users'][0];
                     
-                }
-                catch (e){
-                    alert('Error')
-                }
-        },
+        //         }
+        //         catch (e){
+        //             alert('Error')
+        //         }
+        // },
         nameSlice(atribut){
             let s=''
             switch(atribut){
@@ -189,6 +166,11 @@ export default {
             
         }
     },
+    computed: {
+    ...mapState({
+        globalAuthor: (state) => state.report.globalAuthor,
+    }),
+  },
 
     components: { MyInput,
         uploadFile,}
